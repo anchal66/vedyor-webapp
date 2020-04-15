f085b8ae50f6ab5c115a7215a92075fd7d1ee203
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-15 13:30:23 +0530

    bit cnages for rote from url

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  currRoute;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd ) {
+        console.log(event.url);
+        if(event.url === '/'){
+          this.currRoute = 'Home'
+        }
+        if(event.url === '/services'){
+          this.currRoute = 'Our Services'
+        }
+        if(event.url === '/aboutus'){
+          this.currRoute = 'About Us'
+        }
+        if(event.url === '/contactus'){
+          this.currRoute = 'Contact Us'
+        }
+        if(event.url === '/corona'){
+          this.currRoute = 'Corona Virus'
+        }
+        if(event.url === '/solutions'){
+          this.currRoute = 'Explore Our Solutions'
+        }
+      }
+    });
+
   }
 
 }
